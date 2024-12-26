bcf040c000b5f46adbbe896019e8c7675d8368c9
Peterson Andres Sena <petersonsena24@gmail.com>
2024-12-26 17:11:39 +0100

    salary test

---
+++ tests/Unit/Service/Salary/SalaryServiceTest.php
@@ -0,0 +1,54 @@
+<?php 
+namespace Tests\Unit\Service\Salary;
+
+use App\Enums\WorkTypeEnum;
+use App\Services\Salary\SalaryService;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use App\Models\Employee;
+use App\Models\HourWorked;
+use App\Models\HourSession;
+use App\Models\Salary;
+use Carbon\Carbon;
+use Database\Factories\HourWorkedFactory;
+use Database\Factories\HourSessionFactory;
+
+class SalaryServiceTest extends TestCase{
+    use DatabaseTransactions;
+    private SalaryService $salaryService;
+    private Employee $employee;
+    private HourWorked $hourWorked;
+    private HourSession $hourSession;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->employee = Employee::factory()->create();
+        $this->hourSession = HourSession::factory()->create(['employee_id' => $this->employee->id,
+            'date' => '2023-01-19',
+            'start_time' => '09:00',
+            'end_time' => '17:00',
+            'planned_hours' => 8,
+            'work_type' => WorkTypeEnum::NORMAL->value]);
+        $this->hourWorked = HourWorked::factory()->create(['hour_session_id' => $this->hourSession->id,
+            'normal_hours' => 8,
+            'overtime_hours' => 0,
+            'holiday_hours' => 0]);
+        $this->salaryService = new SalaryService();
+    }
+
+    public function testCantInstantiate(): void{
+        $this->assertInstanceOf(SalaryService::class, $this->salaryService);
+    }
+
+    public function test_salary_service_execute(){
+        $this->salaryService->execute($this->employee->id, '2023-01-19');
+        $this->assertDatabaseHas('salaries', [
+            'employee_id' => $this->employee->id,
+            'total_normal_hours' => 8,
+            'total_overtime_hours' => 0,
+            'total_holiday_hours' => 0,
+            'total_net_salary' => 0
+        ]);
+    }
+}
