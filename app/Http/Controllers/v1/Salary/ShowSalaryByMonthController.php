633d80712c4821a265566885a465bce7eace3abb
Peterson Andres Sena <petersonsena24@gmail.com>
2024-12-26 14:28:10 +0100

    Cambio de resquest body a query param

--- app/Http/Controllers/v1/Salary/ShowSalaryByMonthController.php
+++ app/Http/Controllers/v1/Salary/ShowSalaryByMonthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\v1\Salary;
 
 use App\Exceptions\SalaryNotFoundException;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SalaryByMonthRequest;
 use App\Services\Salary\FindSalaryByMonthService;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
@@ -20,11 +21,11 @@ class ShowSalaryByMonthController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return mixed|\Illuminate\Http\JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(SalaryByMonthRequest $request)
     {
         try{
             $employee = $request->user()->employee;
-            $salary = $this->findSalaryByMonthService->execute(   $employee->id, $request->month, $request->year);
+            $salary = $this->findSalaryByMonthService->execute(   $employee->id, $request->query('month'), $request->query('year'));
             return response()->json(['salary' => $salary], 200);
         }catch(SalaryNotFoundException $e){
             throw new HttpResponseException(response()->json(['message' => $e->getMessage()], $e->getCode()));
