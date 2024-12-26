ec592b557131e47c0e9588f4c5c352d6af46ef7f
Peterson Andres Sena <petersonsena24@gmail.com>
2024-12-26 17:01:00 +0100

    HourWorkedFactory

---
+++ database/factories/HourWorkedFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\HourSession;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\HourWorked>
+ */
+class HourWorkedFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $hourSession = HourSession::factory()->create();
+        return [
+            'hour_session_id' => $hourSession->id,
+            'normal_hours' => $this->faker->numberBetween(1, 10),
+            'overtime_hours' => 0,
+            'holiday_hours' => 0
+        ];
+    }
+}
