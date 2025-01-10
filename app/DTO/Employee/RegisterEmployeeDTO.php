9d9677739f7c01b38ab2a9357ebed478b86cfae8
Peterson Andres Sena <petersonsena24@gmail.com>
2025-01-10 18:12:37 +0100

    cambiando nombre a employeeDTO a RegisterEmployeeDTO

--- app/DTO/Employee/RegisterEmployeeDTO.php
+++ app/DTO/Employee/RegisterEmployeeDTO.php
@@ -5,22 +5,25 @@ declare(strict_types=1);
 namespace App\DTO\Employee;
 
 use App\DTO\DTOInterface;
+use App\DTO\User\UserDTO;
 use Illuminate\Database\Eloquent\Model;
 
-class EmployeeDTO implements DTOInterface
+class RegisterEmployeeDTO implements DTOInterface
 {
     public function __construct(
+        public UserDTO $user,
         public string $name,
-        public string $company_name,
+        public ?string $company_name,
         public float $normal_hourly_rate,
         public float $overtime_hourly_rate,
         public float $holiday_hourly_rate,
-        public float $irpf
+        public ?float $irpf
     ) {}
 
     public static function fromModel(Model $employee): self
     {
         return new self(
+            UserDTO::fromModel($employee->user),
             $employee->name,
             $employee->company_name ?? '',
             (float) $employee->normal_hourly_rate,
@@ -33,8 +36,10 @@ class EmployeeDTO implements DTOInterface
     public static function toArray(array $data): array
     {
         return [
+            'user' => UserDTO::toArray($data),
+
             'name' => $data['name'],
-            'company_name' => $data['company_name'],
+            'company_name' => $data['company_name']?? '',
             'normal_hourly_rate' => $data['normal_hourly_rate'],
             'overtime_hourly_rate' => $data['overtime_hourly_rate'],
             'holiday_hourly_rate' => $data['holiday_hourly_rate'],
