67bc4ad3d63d065a74a06d84838d3817f4782339
Peterson Sena <petesonsena24@gmail.com>
2025-01-14 23:34:12 +0100

    register user dto

--- app/DTO/User/RegisterUserDTO.php
+++ app/DTO/User/RegisterUserDTO.php
@@ -7,20 +7,20 @@ namespace App\DTO\User;
 use App\DTO\DTOInterface;
 use Illuminate\Database\Eloquent\Model;
 
-readonly class UserDTO implements DTOInterface
+readonly class RegisterUserDTO implements DTOInterface
 {
     public function __construct(
-        public string $id,
         public string $email,
+        public string $password
 
     ) {}
 
-    public static function fromModel(Model $object): UserDTO
+    public static function fromModel(Model $object): RegisterUserDTO
     {
 
         return new self(
-            $object->id,
             $object->email,
+            $object->password
 
         );
 
@@ -30,8 +30,8 @@ readonly class UserDTO implements DTOInterface
     {
 
         return [
-            'id' => $data['id']?? null,
             'email' => $data['email'],
+            'password' => $data['password']
         ];
     }
 }
