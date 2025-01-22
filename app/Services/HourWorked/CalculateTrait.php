377958c98f850e4e03d9c110b2262160345ec76e
Peterson Sena <petesonsena24@gmail.com>
2025-01-22 09:58:49 +0100

    metodo verifyDuration

--- app/Services/HourWorked/CalculateTrait.php
+++ app/Services/HourWorked/CalculateTrait.php
@@ -25,7 +25,7 @@ trait CalculateTrait
         $start = Carbon::parse($startTime);
         $end = Carbon::parse($endTime);
 
-        $hoursWorkedCalculated = $this->verifyDuration($start, $end);
+        $hoursWorkedCalculated = $this->diffInHours($start, $end);
         // Verificar si es festivo y calcular las horas festivas
         $holidayHours = $this->calculateHolidayHours($hoursWorkedCalculated, $workType);
         // Calcular horas extras
@@ -44,6 +44,13 @@ trait CalculateTrait
             'overtimeHours' => $regularOvertimeHours + $extraShiftHours,
             'holidayHours' => $holidayHours,
         ];
+    }
+
+    private function diffInHours($start, $end)
+    {
+        $start = Carbon::parse($start);
+        $end = Carbon::parse($end);
+        return (float) $start->floatDiffInHours($end);
     }
 
     /**
@@ -66,7 +73,7 @@ trait CalculateTrait
         if ($end <= $start || $start > $end) {
             throw new TimeEntryException('The start time cannot be greater than the end time');
         }
-        $hoursWorkedCalculated = $start->floatDiffInHours($end);
+        $hoursWorkedCalculated = $this->diffInHours($start, $end);
 
         if ($hoursWorkedCalculated >= $maxHoursWorked || $hoursWorkedCalculated < $minHoursWorked) {
             throw new TimeEntryException(
