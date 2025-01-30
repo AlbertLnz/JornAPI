773210a3ba613f1de39c8c9100a97f34011e99c5
Peterson Sena <petesonsena24@gmail.com>
2025-01-30 23:23:44 +0100

    env docker-compose, validacion seeder

--- docker/scripts/artisan.sh
+++ docker/scripts/artisan.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Ejecutando comandos de Artisan..."
-php /var/www/html/artisan migrate --force
+
 php /var/www/html/artisan cache:clear
 php /var/www/html/artisan config:clear
+php /var/www/html/artisan route:clear
+php /var/www/html/artisan db:seed 
+echo "Comandos de Artisan ejecutados."
